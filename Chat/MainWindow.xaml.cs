f763b8d370e7e044be8fe23a57bb3669901d19dd
WinuX <37900754+xWinuX@users.noreply.github.com>
2022-03-28 15:37:09 +0200

    Created repo + tested out wpf

---
+++ Chat/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace Chat
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            ChangeLabel.Content = "Thaksdjkasd";
+        }
+    }
+}
